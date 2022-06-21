31806aa9b772e52f50e1087daf9633b97b99f941
Erik Eiesland <erik@eies.land>
2022-06-21 09:48:32 +0200

    kjørende dev-versjon av client

---
+++ client/src/GlobalStyle.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  main {
+    width: 80rem;
+    margin: 0 auto;
+  }
+`
---
+++ client/src/main.tsx
@@ -0,0 +1,15 @@
+import '@navikt/ds-css'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
+import { GlobalStyle } from './GlobalStyle'
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
