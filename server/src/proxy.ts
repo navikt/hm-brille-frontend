6eb882a90fd0c2ed57fd1551dabeb117518e7b29
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-06-27 13:14:44 +0200

    Remove stale debug logging

--- server/src/proxy.ts
+++ server/src/proxy.ts
@@ -12,13 +12,9 @@ function createProxy(
     parseReqBody: false,
     async proxyReqOptDecorator(requestOptions, req) {
       const { access_token } = await exchangeIDPortenToken(req, targetAudience)
-      console.log(`DEBUG: DEBUG: token generated through exchange: ${access_token}`)
       requestOptions.headers = {
         ...requestOptions.headers,
         Authorization: `Bearer ${access_token}`,
-      }
-      if (config.nais_cluster_name === 'dev-gcp') {
-        // requestOptions.headers['x-optiker-fnr'] = '01117302624'
       }
       return requestOptions
     },
