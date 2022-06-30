4e79749d50fc22f09029575ea1eef37d497fd079
Erik Eiesland <erik@eies.land>
2022-06-30 14:20:10 +0200

    fjerner bekreftelser

--- client/src/søknad/SøknadForm.tsx
+++ client/src/søknad/SøknadForm.tsx
@@ -1,5 +1,5 @@
-import { Calender, Delete } from '@navikt/ds-icons'
-import { Button, ConfirmationPanel, Heading, Panel, Radio, RadioGroup, TextField } from '@navikt/ds-react'
+import { Delete } from '@navikt/ds-icons'
+import { Button, Heading, Radio, RadioGroup, TextField } from '@navikt/ds-react'
 import { Controller, useForm } from 'react-hook-form'
 import styled from 'styled-components'
 import { Avstand } from '../components/Avstand'
@@ -38,24 +38,26 @@ export function SøknadForm() {
       <Heading level="2" size="medium">
         Om brillen
       </Heading>
-      <TextField label="Hvilken dato ble brillen bestilt?" description="DD.MM.ÅÅÅÅ" {...register('kjøpsdato')} />
-      <TextField label="Bestillingsreferanse" {...register('referansenummer')} />
-      <Avstand paddingBottom={5} paddingTop={5}>
-      <ConfirmationPanel checked={false} size="medium" label="Ja, jeg bekrefter dette.">
-        Brillen er ikke av type solbrille eller sportsbrille
-      </ConfirmationPanel>
-      </Avstand>
-      <Avstand paddingBottom={3} paddingTop={3}>
-      <ConfirmationPanel checked={false} size="medium" label="Ja, jeg bekrefter dette.">
-        Brillen skal ikke brukes i behandling eller forebygging av amblyopi.
-      </ConfirmationPanel>
-      </Avstand>
       <Avstand paddingBottom={3} paddingTop={3}>
         <form
           onSubmit={handleSubmit(async (data) => {
             console.log(data)
           })}
         >
+          <Avstand marginBottom={3}>
+            <TextField label="Hvilken dato ble brillen bestilt?" description="DD.MM.ÅÅÅÅ" {...register('kjøpsdato')} />
+          </Avstand>
+          <Avstand marginBottom={3}>
+            <TextField
+              label="Hvilken dato ble synsundersøkelse gjennomført?"
+              description="DD.MM.ÅÅÅÅ"
+              {...register('kjøpsdato')}
+            />
+          </Avstand>
+          <Avstand marginBottom={3}>
+            <TextField label="Bestillingsreferanse" {...register('referansenummer')} />
+          </Avstand>
+
           <Heading level="2" size="large" spacing>
             Informasjon om optiker/virksomhet
           </Heading>
@@ -64,10 +66,11 @@ export function SøknadForm() {
             name="virksomhet"
             control={control}
             render={({ field }) => (
-              <RadioGroup legend="Hvilken virksomhet representerer du?"  {...field}>
-                <Radio value="1" checked={true}>Brilleland Sandaker</Radio>
-                <Radio value="Annen"  >Annen</Radio>
-
+              <RadioGroup legend="Hvilken virksomhet representerer du?" {...field}>
+                <Radio value="1" checked={true}>
+                  Brilleland Sandaker
+                </Radio>
+                <Radio value="Annen">Annen</Radio>
               </RadioGroup>
             )}
           />
