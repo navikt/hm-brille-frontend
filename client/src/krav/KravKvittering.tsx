0e59d7a32defdf0ee55421f132b2c2dfc7bbfb2e
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-07-27 12:59:54 +0200

    Viser kun hotjar på kvitteringsside

--- client/src/krav/KravKvittering.tsx
+++ client/src/krav/KravKvittering.tsx
@@ -1,52 +1,56 @@
-import { Alert, BodyLong, Heading } from '@navikt/ds-react'
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Avstand } from '../components/Avstand'
-import { Beløp } from '../components/Beløp'
-import { Data } from '../components/Data'
-import { Dato } from '../components/Dato'
-import { Datum } from '../components/Datum'
-import { organisasjonsnummer } from '../components/organisasjonsnummer'
+import {Alert, BodyLong, Heading} from '@navikt/ds-react'
+import {useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {Avstand} from '../components/Avstand'
+import {Beløp} from '../components/Beløp'
+import {Data} from '../components/Data'
+import {Dato} from '../components/Dato'
+import {Datum} from '../components/Datum'
+import {organisasjonsnummer} from '../components/organisasjonsnummer'
 import ScrollToTop from '../components/ScrollToTop'
-import { useApplicationContext } from '../state/ApplicationContext'
-import { OpprettKravResponse } from '../types'
-import { useLocationState } from '../useLocationState'
-import { KravSteg } from './KravSteg'
+import {useApplicationContext} from '../state/ApplicationContext'
+import {OpprettKravResponse} from '../types'
+import {useLocationState} from '../useLocationState'
+import {KravSteg} from './KravSteg'
+import {HotjarTrigger} from "../components/hotjar-trigger";
 
 export function KravKvittering() {
-  const { resetAppState } = useApplicationContext()
-  const state = useLocationState<OpprettKravResponse>()
-  useEffect(() => {
-    resetAppState()
-  }, [])
-  if (!state) {
-    return null
-  }
-  const { id, orgnr, bestillingsreferanse, beløp, opprettet } = state
-  return (
-    <KravSteg>
-      <ScrollToTop />
-      <Alert variant="success">Kravet er registrert</Alert>
-      <Avstand marginBottom={5} />
-      <BodyLong spacing>
-        Kravet om direkte oppgjør er automatisk registrert. NAV utbetaler stønaden til firmaets kontonummer senest 30
-        dager etter at kravet er registrert.
-      </BodyLong>
-      <Heading level="2" size="medium">
-        Kvittering
-      </Heading>
-      <Data labelColumnWidth={150}>
-        <Datum label="Org. nummer">{organisasjonsnummer(orgnr)}</Datum>
-        <Datum label="Innsendt dato">
-          <Dato verdi={opprettet}></Dato>
-        </Datum>
-        <Datum label="Beløp til utbetaling">
-          <Beløp verdi={beløp} />
-        </Datum>
-        <Datum label="Deres referansenr.">{bestillingsreferanse}</Datum>
-        <Datum label="NAVs referansenr.">{id}</Datum>
-      </Data>
-      <Link to="/">Til forsiden</Link>
-    </KravSteg>
-  )
+    const {resetAppState} = useApplicationContext()
+    const state = useLocationState<OpprettKravResponse>()
+    useEffect(() => {
+        resetAppState()
+    }, [])
+    if (!state) {
+        return null
+    }
+    const {id, orgnr, bestillingsreferanse, beløp, opprettet} = state
+    return (
+        <HotjarTrigger>
+            <KravSteg>
+                <ScrollToTop/>
+                <Alert variant="success">Kravet er registrert</Alert>
+                <Avstand marginBottom={5}/>
+                <BodyLong spacing>
+                    Kravet om direkte oppgjør er automatisk registrert. NAV utbetaler stønaden til firmaets kontonummer
+                    senest 30
+                    dager etter at kravet er registrert.
+                </BodyLong>
+                <Heading level="2" size="medium">
+                    Kvittering
+                </Heading>
+                <Data labelColumnWidth={150}>
+                    <Datum label="Org. nummer">{organisasjonsnummer(orgnr)}</Datum>
+                    <Datum label="Innsendt dato">
+                        <Dato verdi={opprettet}></Dato>
+                    </Datum>
+                    <Datum label="Beløp til utbetaling">
+                        <Beløp verdi={beløp}/>
+                    </Datum>
+                    <Datum label="Deres referansenr.">{bestillingsreferanse}</Datum>
+                    <Datum label="NAVs referansenr.">{id}</Datum>
+                </Data>
+                <Link to="/">Til forsiden</Link>
+            </KravSteg>
+        </HotjarTrigger>
+    )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,6 @@ import { App } from './App'
 import { GlobalStyle } from './GlobalStyle'
 import { baseUrl, http } from './http'
 import { initMSW } from './mocks/initMSW'
-import {HotjarTrigger} from "./components/hotjar-trigger";
 
 const swrConfig: SWRConfiguration = {
   async fetcher(url: string) {
@@ -26,9 +25,7 @@ initMSW().then(() => {
       <GlobalStyle />
       <SWRConfig value={swrConfig}>
         <BrowserRouter basename={baseUrl()}>
-            <HotjarTrigger>
-                <App />
-            </HotjarTrigger>
+          <App />
         </BrowserRouter>
       </SWRConfig>
     </React.StrictMode>
