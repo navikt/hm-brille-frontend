42718b67478681d8e726dbe6f7fc2b78928bb9fd
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-07-27 12:41:39 +0200

    La til hotjar

---
+++ client/src/components/hotjar-trigger.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+
+declare global {
+    interface Window {
+        hj: any
+    }
+}
+
+interface HotjarTriggerProps {
+    children: any;
+}
+
+export const HotjarTrigger = ({ children }: HotjarTriggerProps) => {
+    useEffect(() => {
+        window.hj =
+            window.hj ||
+            function () {
+                ;(window.hj.q = window.hj.q || []).push(arguments)
+            }
+        if (window.appSettings.MILJO !== 'labs-gcp' && window.appSettings.MILJO !== 'dev-gcp') {
+            window.hj('trigger', 'digihot_hm_brille_api_krav')
+        }
+    })
+
+    return children
+}
