28b28a39d0c39bdab969799e47c7870fe592829b
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2022-07-28 10:17:32 +0200

    logging for å debugge hotjar

--- client/src/components/hotjar-trigger.tsx
+++ client/src/components/hotjar-trigger.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import {useEffect} from 'react'
 import {useLocation} from "react-router-dom";
 
 declare global {
@@ -11,23 +11,21 @@ interface HotjarTriggerProps {
     children: any;
 }
 
-export const HotjarTrigger = ({ children }: HotjarTriggerProps) => {
+export const HotjarTrigger = ({children}: HotjarTriggerProps) => {
 
     const location = useLocation()
 
     useEffect(() => {
         console.log(`location: ${location.pathname}`)
-        if(location.pathname === "/krav/kvittering"){
-            window.hj =
-                window.hj ||
-                function () {
-                    ;(window.hj.q = window.hj.q || []).push(arguments)
-                }
-            if (window.appSettings.MILJO !== 'labs-gcp') {
-                window.hj('trigger', 'digihot_hm_brille_api_krav')
+        window.hj =
+            window.hj ||
+            function () {
+                ;(window.hj.q = window.hj.q || []).push(arguments)
             }
+        if (window.appSettings.MILJO !== 'labs-gcp' && location.pathname === "/krav/kvittering") {
+            console.log(`HotJar trigger`)
+            window.hj('trigger', 'digihot_hm_brille_api_krav')
         }
-
     })
 
     return children
