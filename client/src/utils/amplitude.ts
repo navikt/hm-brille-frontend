ba091f9eae52a7841fcff27978c6692a7421f095
Håkon <hakonhagen2@gmail.com>
2022-08-03 11:03:55 +0200

    post merge fixes

--- client/src/utils/amplitude.ts
+++ client/src/utils/amplitude.ts
@@ -2,23 +2,23 @@ import amplitude from 'amplitude-js'
 import { v4 as uuidv4 } from 'uuid'
 
 export enum amplitude_taxonomy {
-    SKJEMA_START = 'skjema startet',
-    SKJEMA_ÅPEN = 'skjema åpnet',
-    SKJEMASTEG_FULLFØRT = 'skjemasteg fullført',
-    SKJEMAVALIDERING_FEILET = 'skjemavalidering feilet',
-    SKJEMAINNSENDING_FEILET = 'skjemainnsending feilet',
-    SKJEMA_FULLFØRT = 'skjema fullført',
-    NAVIGERE = 'navigere',
+  SKJEMA_START = 'skjema startet',
+  SKJEMA_ÅPEN = 'skjema åpnet',
+  SKJEMASTEG_FULLFØRT = 'skjemasteg fullført',
+  SKJEMAVALIDERING_FEILET = 'skjemavalidering feilet',
+  SKJEMAINNSENDING_FEILET = 'skjemainnsending feilet',
+  SKJEMA_FULLFØRT = 'skjema fullført',
+  NAVIGERE = 'navigere',
 }
 
 //Events som ikke er i NAV sin taxonomi
 export enum digihot_customevents {
-    VILKÅRSVURDERING_RESULTAT = 'vilkårsvurdering resultat',
+  VILKÅRSVURDERING_RESULTAT = 'vilkårsvurdering resultat',
+  KVITTERING_PRINT_ÅPNET = 'åpning av printmodal for kvittering',
 }
 
-
 export enum SkjemaSteg {
-    KRAV = 10,
+  KRAV = 10,
 }
 
 const SKJEMANAVN = 'hm-brille-frontend'
@@ -26,78 +26,83 @@ const SKJEMANAVN = 'hm-brille-frontend'
 let skjemaId = uuidv4()
 
 export const initAmplitude = () => {
-    if (amplitude) {
-        amplitude.getInstance().init('default', '', {
-            apiEndpoint: 'amplitude.nav.no/collect-auto',
-            saveEvents: false,
-            includeUtm: true,
-            includeReferrer: true,
-            platform: window.location.toString(),
-        })
-    }
+  if (amplitude) {
+    amplitude.getInstance().init('default', '', {
+      apiEndpoint: 'amplitude.nav.no/collect-auto',
+      saveEvents: false,
+      includeUtm: true,
+      includeReferrer: true,
+      platform: window.location.toString(),
+    })
+  }
 }
 
 export function logAmplitudeEvent(eventName: string, data?: any) {
-    setTimeout(() => {
-        data = {
-            app: SKJEMANAVN,
-            team: 'teamdigihot',
-            ...data,
-        }
-        try {
-            if (amplitude) {
-                amplitude.getInstance().logEvent(eventName, data)
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    })
+  setTimeout(() => {
+    data = {
+      app: SKJEMANAVN,
+      team: 'teamdigihot',
+      ...data,
+    }
+    try {
+      if (amplitude) {
+        amplitude.getInstance().logEvent(eventName, data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  })
 }
 
 export function logCustomEvent(event: digihot_customevents, data?: any) {
-    logAmplitudeEvent(event, {
-        skjemanavn: SKJEMANAVN,
-        ...data,
-    })
+  logAmplitudeEvent(event, {
+    skjemanavn: SKJEMANAVN,
+    ...data,
+  })
 }
 
 export function logNavigeringLenke(destinasjon: string): void {
-    logAmplitudeEvent(amplitude_taxonomy.NAVIGERE, {
-        skjemanavn: SKJEMANAVN,
-        destinasjon: destinasjon,
-    })
+  logAmplitudeEvent(amplitude_taxonomy.NAVIGERE, {
+    skjemanavn: SKJEMANAVN,
+    destinasjon: destinasjon,
+  })
 }
 
-
 export function logSkjemaStartet() {
-    skjemaId = uuidv4()
-    logAmplitudeEvent(amplitude_taxonomy.SKJEMA_START, {
-        skjemanavn: SKJEMANAVN,
-        skjemaId: skjemaId,
-        steg: 0,
-    })
+  skjemaId = uuidv4()
+  logAmplitudeEvent(amplitude_taxonomy.SKJEMA_START, {
+    skjemanavn: SKJEMANAVN,
+    skjemaId: skjemaId,
+    steg: 0,
+  })
 }
 
 export function logSkjemastegFullfoert(steg: SkjemaSteg) {
-    logAmplitudeEvent(amplitude_taxonomy.SKJEMASTEG_FULLFØRT, {
-        skjemanavn: SKJEMANAVN,
-        skjemaId: skjemaId,
-        steg: steg,
-    })
+  logAmplitudeEvent(amplitude_taxonomy.SKJEMASTEG_FULLFØRT, {
+    skjemanavn: SKJEMANAVN,
+    skjemaId: skjemaId,
+    steg: steg,
+  })
 }
 
-
 export function logSkjemaFullfoert() {
-    logAmplitudeEvent(amplitude_taxonomy.SKJEMA_FULLFØRT, {
-        skjemanavn: SKJEMANAVN,
-        skjemaId: skjemaId,
-    })
+  logAmplitudeEvent(amplitude_taxonomy.SKJEMA_FULLFØRT, {
+    skjemanavn: SKJEMANAVN,
+    skjemaId: skjemaId,
+  })
 }
 
 export function logSkjemavalideringFeilet(feilmeldinger: string[] | undefined) {
-    logAmplitudeEvent(amplitude_taxonomy.SKJEMAVALIDERING_FEILET, {
-      skjemanavn: SKJEMANAVN,
-      skjemaId: skjemaId,
-      feilmeldinger: feilmeldinger,
-    })
-  }
+  logAmplitudeEvent(amplitude_taxonomy.SKJEMAVALIDERING_FEILET, {
+    skjemanavn: SKJEMANAVN,
+    skjemaId: skjemaId,
+    feilmeldinger: feilmeldinger,
+  })
+}
+
+export function logPrintÅpnet() {
+  logAmplitudeEvent(digihot_customevents.KVITTERING_PRINT_ÅPNET, {
+    skjemanavn: SKJEMANAVN,
+    skjemaId: skjemaId,
+  })
+}
