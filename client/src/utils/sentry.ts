1835259d9fbff7f487b6cf2bab857274df30cf9b
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-10-29 10:01:54 +0100

    Sync sentry versions

--- client/src/utils/sentry.ts
+++ client/src/utils/sentry.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/react'
-import { BrowserTracing } from '@sentry/tracing'
+import { browserTracingIntegration } from "@sentry/react";
 
 const MILJO = window.appSettings?.MILJO
 const USE_MSW = window.appSettings?.USE_MSW
@@ -7,7 +7,7 @@ const USE_MSW = window.appSettings?.USE_MSW
 export function initSentry() {
   Sentry.init({
     dsn: 'https://2c6044e4012c41f8b7b6000569dbf08d@sentry.gc.nav.no/165',
-    integrations: [new BrowserTracing()],
+    integrations: [browserTracingIntegration()],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
